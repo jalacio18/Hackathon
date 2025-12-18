1f9dd51e339c14746614e2bfa312d1199efacec0
javierortegalucena <javi.ortega.lucena@gmail.com>
2025-12-18 11:27:34 +0100

    Update Main.py

--- Main.py
+++ Main.py
@@ -39,3 +39,71 @@ TIEMPO_SOBREGIRO      = 0.4
 
     tiempo_inicio_giro = sim.getSimulationTime()
 
+while sim.getSimulationTime() < 180:
+
+        lectura_frontal   = sim.readProximitySensor(sensor_frontal)
+        lectura_derecha   = sim.readProximitySensor(sensor_derecho)
+        lectura_izquierda = sim.readProximitySensor(sensor_izquierdo)
+
+        distancia_frontal   = lectura_frontal[1]   if lectura_frontal[0]   else 5.0
+        distancia_derecha   = lectura_derecha[1]   if lectura_derecha[0]   else 5.0
+        distancia_izquierda = lectura_izquierda[1] if lectura_izquierda[0] else 5.0
+
+        tiempo_actual = sim.getSimulationTime()
+
+        if estado_actual == AVANZAR_RECTO:
+
+            error_lateral = DISTANCIA_OBJETIVO - distancia_izquierda
+            correccion = max(min(error_lateral, 0.6), -0.6)
+
+            sim.setJointTargetVelocity(
+                motor_izquierdo, VELOCIDAD_AVANCE - correccion
+            )
+            sim.setJointTargetVelocity(
+                motor_derecho, VELOCIDAD_AVANCE + correccion
+            )
+
+            if distancia_frontal < DISTANCIA_PARED:
+
+                sim.setJointTargetVelocity(motor_izquierdo, 0)
+                sim.setJointTargetVelocity(motor_derecho, 0)
+                sim.step()
+
+                if distancia_izquierda < DISTANCIA_PARED and distancia_derecha < DISTANCIA_PARED:
+                    estado_actual = GIRAR_180_GRADOS
+                elif distancia_derecha < DISTANCIA_PARED:
+                    estado_actual = GIRAR_IZQUIERDA
+                elif distancia_izquierda < DISTANCIA_PARED:
+                    estado_actual = GIRAR_DERECHA
+                else:
+                    estado_actual = GIRAR_IZQUIERDA
+
+                tiempo_inicio_giro = tiempo_actual
+
+        elif estado_actual == GIRAR_IZQUIERDA:
+
+            sim.setJointTargetVelocity(motor_derecho,  VELOCIDAD_GIRO)
+            sim.setJointTargetVelocity(motor_izquierdo, -VELOCIDAD_GIRO)
+
+            if tiempo_actual - tiempo_inicio_giro > TIEMPO_GIRO_90_GRADOS + TIEMPO_SOBREGIRO:
+                estado_actual = AVANZAR_RECTO
+
+        elif estado_actual == GIRAR_DERECHA:
+
+            sim.setJointTargetVelocity(motor_derecho, -VELOCIDAD_GIRO)
+            sim.setJointTargetVelocity(motor_izquierdo,  VELOCIDAD_GIRO)
+
+            if tiempo_actual - tiempo_inicio_giro > TIEMPO_GIRO_90_GRADOS + TIEMPO_SOBREGIRO:
+                estado_actual = AVANZAR_RECTO
+
+        elif estado_actual == GIRAR_180_GRADOS:
+
+            sim.setJointTargetVelocity(motor_derecho, -1.6)
+            sim.setJointTargetVelocity(motor_izquierdo,  1.6)
+
+            if tiempo_actual - tiempo_inicio_giro > 3.0:
+             estado_actual = AVANZAR_RECTO
+
+        sim.step()
+
+
