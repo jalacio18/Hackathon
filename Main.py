28f2066537eef2a443565982a03c73127c57e9d6
Giovanni Deana Corral <gdeanacorral@gmail.com>
2025-12-18 11:25:36 +0100

    Reformat Main.py for consistency and clarity

--- Main.py
+++ Main.py
@@ -18,3 +18,24 @@ DISTANCIA_OBJETIVO = 0.45
 # TIEMPOS DE GIRO (s)
 TIEMPO_GIRO_90_GRADOS = 1.0
 TIEMPO_SOBREGIRO      = 0.4
+ def main():
+
+    estado_actual = AVANZAR_RECTO
+
+    client = RemoteAPIClient()
+    sim = client.require('sim')
+
+    sim.setStepping(True)
+    sim.startSimulation()
+
+    # MOTORES
+    motor_derecho   = sim.getObject("/PioneerP3DX/rightMotor")
+    motor_izquierdo = sim.getObject("/PioneerP3DX/leftMotor")
+
+    # SENSORES ULTRASÓNICOS
+    sensor_frontal   = sim.getObject("/PioneerP3DX/ultrasonicSensor[3]")
+    sensor_derecho   = sim.getObject("/PioneerP3DX/ultrasonicSensor[2]")
+    sensor_izquierdo = sim.getObject("/PioneerP3DX/ultrasonicSensor[5]")
+
+    tiempo_inicio_giro = sim.getSimulationTime()
+
